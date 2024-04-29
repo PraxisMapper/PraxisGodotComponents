820b6061794c44ed676d4544c5d39cf25663f1b4
PraxisMapper <praxismapper@gmail.com>
2024-04-29 18:40:28 -0400

    remove debug line

--- Scenes/SimpleTest.gd
+++ Scenes/SimpleTest.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _ready():
 	#Working
 	#await $MinOfflineTiles.GetAndProcessData("85633Q", "suggestedmini")
-	#await $FullOfflineTiles.GetAndProcessData("85633Q", 1) #can be 6 or 8 characters long
+	await $FullOfflineTiles.GetAndProcessData("85633Q", 4) #can be 6 or 8 characters long
 	#var test3 = await PraxisOfflineData.GetPlacesPresent("85633QGP5M")
 	#print(test3)
 	#in testing now
