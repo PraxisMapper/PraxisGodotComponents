feefc7b0d2d0bd491940a70c472309f3620e7c73
PraxisMapper <praxismapper@gmail.com>
2024-06-14 00:07:16 -0400

    Test scene for proxyplay and scrolling map

---
+++ Scenes/ProxyPlayTest.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+#TODO:
+#When entered, set proxyPlay to true, pick a base location, and grab the next/last
+#position as the proxyBase.
+#When closing, set proxyPlay to false.
+#May pick a map location to use to with offline tile data
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$lblProxy.text = "Proxied to: " + PraxisCore.currentPlusCode
+	PraxisCore.plusCode_changed.connect(onPluscodeChanged)
+	
+	#NOTE: To center the 5x5 scrolling map on a 1080p screen
+	#set its position to -260, -290
+
+func onPluscodeChanged(current, _old):
+	$lblProxy.text = "Proxied to: " + current
+
+
+func Close():
+	PraxisCore.SetProxyPlay(false)
+	get_tree().change_scene_to_file("res://Scenes/SimpleTest.tscn")
+	#Whatever returns us to the main scene.
