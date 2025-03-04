c1cd67e48f69c7e08910204a542bd889a92e8365
PraxisMapper <praxismapper@gmail.com>
2025-03-04 14:54:17 -0500

    include sensor data in gps test scene

--- Scenes/GpsTest.gd
+++ Scenes/GpsTest.gd
@@ -19,8 +19,31 @@ func print_location_info(location: Dictionary) -> void:
 	text += "Time: " + str(location["time"])
 	text += "\n\n\n"
 	text += "PlusCode:" + PlusCodes.EncodeLatLon(location["latitude"], location["longitude"])
-	
 	lb_location_info.text = text
+
+func SensorData():
+	var text = ""
+	var gravity: Vector3 = Input.get_gravity()
+	text += "Gravity: " + str(gravity) + " m/s"
+	text += "\n"
+	var pitch = atan2(gravity.z, -gravity.y) #rotation.x
+	text += "Pitch: " + str(pitch)
+	text += "\n"
+	var roll = atan2(-gravity.x, -gravity.y)  #rotation.z
+	text += "Roll: " + str(roll)
+	text += "\n"
+	var magnet = Input.get_magnetometer().rotated(-Vector3.FORWARD, roll).rotated(Vector3.RIGHT, pitch)
+	text += "Magnet: " + str(magnet)
+	text += "\n"
+	
+	var yaw_magnet = atan2(-magnet.x, magnet.z)
+	text += "Heading: " + str(-yaw_magnet) # negative to match Godot rotations
+	text += "\n"
+	text += "Heading (Degrees): " + str(rad_to_deg(-yaw_magnet))
+	$lblSensorInfo.text = text
+
+func _process(delta):
+	SensorData()
 
 func _ready():
 	if PraxisCore.last_location != null:
